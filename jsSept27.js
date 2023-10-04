8818167c2f8746e518cbf0f246b294394c09fd1d
Liliia Pavliuchenkova <liliiapavliuchenkova@Liliias-MBP.lan>
2023-10-04 12:02:51 -0700

    SignUp form

---
+++ jsSept27.js
@@ -0,0 +1,31 @@
+let name = prompt('Enter your name: ');
+/*
+
+if (num==='5' || num ==='3') {
+    alert('You guessed a right number');
+}
+else {
+    alert("You didn't guess");
+}*/
+
+//(guess == 5) ? alert("You guessed right!") : alert("You guessed wrong!");
+/*
+let tech = window.prompt("Which front-end technology do you enjoy most?");
+switch (tech) {
+case "html":
+     window.alert("HTML is fun and easy to learn!");
+     break;
+case "css":
+     window.alert("CSS is more difficult to pick up than HTML!");
+     break;
+default:
+     window.alert("Great, I like that technology too!");
+}*/
+/*let x = window.prompt("Enter a number between 1 and 10");
+if (x == 5) {
+     window.alert("You guessed the right number!");
+} else {
+    window.alert("You guessed the wrong number...bummer!");
+}*/
+
+(name === "") ? console.log('you forgot to enter your name!') : console.log(`Nice to meet you, ${name}`)
